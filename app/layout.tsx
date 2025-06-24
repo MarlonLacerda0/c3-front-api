38719edc1cf2979b77c61367d413d8b503d6f935
Marlon <marlonlac10@hotmail.com>
2025-06-23 22:01:11 -0300

    Mudança front e migrations prisma

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Projeto C3 - Prisma e Next.js',
 }
 
 export default function RootLayout({
@@ -13,7 +11,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
+    <html lang="pt-BR">
       <body>{children}</body>
     </html>
   )
